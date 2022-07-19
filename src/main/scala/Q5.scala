ebf97a30b059fd7cb8bac8c7eb7a98ecf9196d11
Janith Madarasinghe <janithpm9991@gmail.com>
2022-07-19 07:35:27 +0530

    scala files

--- src/main/scala/Q5.scala
+++ src/main/scala/Q5.scala
@@ -3,18 +3,11 @@ object Q5 {
   val minPerKm__EasyPace = 8
   val minPerKm__TempoPace = 7
 
-  def RunningTime(pace: Array[Int], minPerKm: Int) : Int = {
-    var temp = 0
-    for (d <- pace) temp = temp + (d * minPerKm)
-    temp
-  }
+  def RunningTime(distance: Int, minPerKm: Int) : Int = distance * minPerKm
 
-  def TotalRunningTime(easyPace: Array[Int], tempoPace: Array[Int]): Int = RunningTime(easyPace, minPerKm__EasyPace) + RunningTime(tempoPace, minPerKm__TempoPace)
+  def TotalRunningTime(): Int = RunningTime(2, minPerKm__EasyPace) + RunningTime(3, minPerKm__TempoPace) + RunningTime(2, minPerKm__EasyPace)
 
   def main(args: Array[String]): Unit = {
-    val easy = Array(2, 2)
-    val tempo = Array(3)
-
-    println(TotalRunningTime(easy, tempo))
+    println(TotalRunningTime())
   }
 }
